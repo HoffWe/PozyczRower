313836e190206c3fdb7e8dbc6f10cf2029b3cb4f
HofWe <tmszpilat@gmail.com>
2026-05-21 15:11:25 +0200

    add Rent repository

--- src/main/java/AI2/Model/Rent.java
+++ src/main/java/AI2/Model/Rent.java
@@ -5,9 +5,49 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Rent {
+    private int id;
     private int bikeId;
     private int clientId;
     private LocalDateTime rentDate;
-    private Instant rentTime;
     private LocalDateTime returnTime;
+
+    public Rent(){}
+    public Rent(int id,int bikeId, int clientId, LocalDateTime rentDate, LocalDateTime returnTime) {
+        this.id = id;
+        this.bikeId = bikeId;
+        this.clientId = clientId;
+        this.rentDate = rentDate;
+        this.returnTime = returnTime;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getBikeId() {
+        return bikeId;
+    }
+    public void setBikeId(int bikeId) {
+        this.bikeId = bikeId;
+    }
+    public int getClientId() {
+        return clientId;
+    }
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+    public LocalDateTime getRentDate() {
+        return rentDate;
+    }
+    public void setRentDate(LocalDateTime rentDate) {
+        this.rentDate = rentDate;
+    }
+    public LocalDateTime getReturnTime() {
+        return returnTime;
+    }
+    public void setReturnTime(LocalDateTime returnTime) {
+        this.returnTime = returnTime;
+    }
+
 }
--- src/main/java/AI2/Service/RentService.java
+++
@@ -1,9 +0,0 @@
-package AI2.Service;
-
-import AI2.Model.Client;
-import AI2.Repository.RentRepository;
-
-public class RentService {
-    private RentRepository rentRepository;
-
-}
